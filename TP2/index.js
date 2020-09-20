62b776b47918d17c95c6fee2bdd81af7e4db0979
Freki <mathieu.chambaud@ynov.com>
2020-09-20 22:34:29 +0200

    dernier commit

--- TP2/index.js
+++ TP2/index.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     addDiv('content')
-    addDiv('menu dark-grey')
 })
 
-function addDiv(name) {
+function addDiv(name, title) {
     let b = document.body
     let newDiv = document.createElement('div')
     newDiv.setAttribute('class', `${name}`)
 
     b.prepend(newDiv)
+
+    newDiv.appendChild()
 }
 
 function addH2(name) {
     let body = document.querySelector('div')
+
+    
 }
